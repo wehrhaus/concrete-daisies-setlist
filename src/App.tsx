4049c465b396b2ca3f7f007cce2dddabe96848d5
Justin Wehrman <jwehrman@wehr.haus>
2024-07-02 00:19:58 -0500

    Quick iteration on displaying a setlist

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Setlist } from "./components/setlist";
+import { setlist07062024 } from "./data/07062024/setlist-07062024.map";
 
 function App() {
   return (
     <>
-      <Setlist />
+      <Setlist setList={setlist07062024}>Hello</Setlist>
     </>
   );
 }
--- src/components/setlist/index.tsx
+++ src/components/setlist/index.tsx
@@ -1,7 +1,40 @@
+import { type Set, type Setlist as SetlistType } from "../../types";
+
 export type SetlistProps = {
   children?: React.ReactNode;
+  setList: SetlistType;
 };
 
 export const Setlist = (props: SetlistProps) => {
-  return <div>{props.children}</div>;
+  // Loop through the setlist which is export type Setlist = Map<string, Set[]>;, write each SET as an <h1> followed by each song in that set's song title as an <h2>
+  const renderSetlist = () => {
+    const setlist: SetlistType = props.setList;
+    const setlistKeys = [...setlist.keys()];
+
+    return setlistKeys.map((setKey) => {
+      const set: Set | undefined = props.setList.get(setKey)?.[0];
+
+      return (
+        <div key={setKey}>
+          <h1>{setKey}</h1>
+          <ul>
+            {set &&
+              set.songs.map((song) => (
+                <li key={song.id}>
+                  <h2>{song.title}</h2>
+                </li>
+              ))}
+          </ul>
+        </div>
+      );
+    });
+  };
+
+  console.log(props.setList);
+  return (
+    <div>
+      {renderSetlist()}
+      {props.children}
+    </div>
+  );
 };
